5d32da12e1152ac32e75e1ad0da24eca3e2f8ce5
amanda-vlopes <amandavitor@hotmail.com>
2023-11-11 17:31:44 -0300

    Requisito 3 finalizado

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -1,4 +1,22 @@
+import csv
+from models.ingredient import Ingredient
+from models.dish import Dish
+
+
 # Req 3
 class MenuData:
     def __init__(self, source_path: str) -> None:
-        pass
+        with open(source_path, encoding="utf-8") as file:
+            dishes_list = list(csv.reader(file, delimiter=",", quotechar='"'))
+            dishes_list.pop(0)
+
+            dishes_set = set()
+            for dish in dishes_list:
+                dish_item = Dish(dish[0], float(dish[1]))
+                dishes_set.add(dish_item)
+                dish_ingredient = Ingredient(dish[2])
+                for dish_set in dishes_set:
+                    if dish_set.name == dish[0]:
+                        dish_set.add_ingredient_dependency(
+                            dish_ingredient, int(dish[3]))
+            self.dishes = dishes_set
